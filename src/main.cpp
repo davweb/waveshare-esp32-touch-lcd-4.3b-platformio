dab8945ce33b305c84f744a74dd68ac22195f16b
David Webb <david@webb.pt>
2026-02-12 09:24:14 +0000

    ✨ Add circle to show touch location

--- src/main.cpp
+++ src/main.cpp
@@ -18,12 +18,12 @@ lv_obj_t* add_row(const char *  labelText, const char * valueText) {
     LOG_DEBUG("Adding row", labelText, valueText);
     lv_obj_t * label = lv_label_create(grid_container);
     lv_label_set_text(label, labelText);
-    lv_obj_set_style_text_font(label, &lv_font_montserrat_24, 0);
+    lv_obj_set_style_text_font(label, &lv_font_montserrat_26, 0);
     lv_obj_set_grid_cell(label, LV_GRID_ALIGN_END, 0, 1, LV_GRID_ALIGN_CENTER, grid_row, 1);
 
     lv_obj_t * value = lv_label_create(grid_container);
     lv_label_set_text(value, valueText);
-    lv_obj_set_style_text_font(value, &lv_font_montserrat_24, 0);
+    lv_obj_set_style_text_font(value, &lv_font_montserrat_26, 0);
     lv_obj_set_grid_cell(value, LV_GRID_ALIGN_START, 1, 1, LV_GRID_ALIGN_CENTER, grid_row, 1);
 
     grid_row += 1;
@@ -33,6 +33,14 @@ lv_obj_t* add_row(const char *  labelText, const char * valueText) {
 
 lv_obj_t* add_row(const char *  labelText, String valueText) {
     return add_row(labelText, valueText.c_str());
+}
+
+static void set_object_opacity(void * obj, int32_t v) {
+    lv_obj_set_style_bg_opa((lv_obj_t *)obj, (lv_opa_t)v, 0);
+}
+
+static void animation_delete_object(lv_anim_t * a) {
+    lv_obj_del((lv_obj_t *)a->var);
 }
 
 static void grid_touch_event_cb(lv_event_t * e)
@@ -55,6 +63,25 @@ static void grid_touch_event_cb(lv_event_t * e)
 
         lvgl_port_lock(-1);
         lv_label_set_text(touchValue, coordinates);
+
+        lv_obj_t * circle = lv_obj_create(lv_scr_act());
+        lv_obj_set_size(circle, 50, 50);
+        lv_obj_set_style_radius(circle, LV_RADIUS_CIRCLE, 0);
+        lv_obj_set_style_bg_color(circle, lv_palette_main(LV_PALETTE_RED), 0);
+        lv_obj_set_style_border_width(circle, 0, 0);
+        lv_obj_set_pos(circle, point.x - 25, point.y - 25);
+        lv_obj_clear_flag(circle, LV_OBJ_FLAG_SCROLLABLE);
+        lv_obj_clear_flag(circle, LV_OBJ_FLAG_CLICKABLE);
+
+        lv_anim_t a;
+        lv_anim_init(&a);
+        lv_anim_set_var(&a, circle);
+        lv_anim_set_values(&a, LV_OPA_COVER, LV_OPA_TRANSP);
+        lv_anim_set_time(&a, 2000);
+        lv_anim_set_exec_cb(&a, set_object_opacity);
+        lv_anim_set_ready_cb(&a, animation_delete_object);
+        lv_anim_start(&a);
+
         lvgl_port_unlock();
     }
 }
@@ -62,7 +89,7 @@ static void grid_touch_event_cb(lv_event_t * e)
 void create_grid_widget(void)
 {
     static lv_coord_t col_dsc[] = {350, LV_GRID_FR(1), LV_GRID_TEMPLATE_LAST};
-    static lv_coord_t row_dsc[] = {30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, LV_GRID_TEMPLATE_LAST};
+    static lv_coord_t row_dsc[] = {33, 33, 33, 33, 33, 33, 33, 33, 33, 33, LV_GRID_TEMPLATE_LAST};
 
     /*Create a container with grid*/
     grid_container = lv_obj_create(lv_scr_act());
@@ -110,10 +137,10 @@ void setup()
     create_grid_widget();
 
     add_row("Chip Model", ESP.getChipModel() + String(" revision ") + String(ESP.getChipRevision()));
-    add_row("SDK Version", ESP.getSdkVersion());
-    add_row("Core Version", ESP.getCoreVersion());
-    add_row("ESP32_Display_Panel Version", getPanelVersion());
-    add_row("LVGL Version", getLvglVersion());
+    add_row("SDK", ESP.getSdkVersion());
+    add_row("Core", ESP.getCoreVersion());
+    add_row("ESP32_Display_Panel", getPanelVersion());
+    add_row("LVGL", getLvglVersion());
     add_row("MAC Address", getMacAddress());
     add_row("IP Address", getLocalIpAddress());
     add_row("Timezone", getTimezone());
@@ -130,7 +157,7 @@ void loop()
     lv_label_set_text(timeValue, getCurrentDateTime().c_str());
     lvgl_port_unlock();
 
-    LOG_DEBUG("Idle loop");
+    LOG_TRACE("Idle loop");
     delay(1000);
 }
 
